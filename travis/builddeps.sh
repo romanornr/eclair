d21a53ebd366e1e416e20f2dae2f89ec137ea043
romanornr <romanornr@gmail.com>
2017-08-10 11:39:20 +0200

    Viacoin: Travis Viacoin binary

--- travis/builddeps.sh
+++ travis/builddeps.sh
@@ -17,9 +17,9 @@ git submodule update
 make
 # bitcoind
 cd
-wget https://bitcoin.org/bin/bitcoin-core-0.13.0/bitcoin-0.13.0-x86_64-linux-gnu.tar.gz
-echo "bcc1e42d61f88621301bbb00512376287f9df4568255f8b98bc10547dced96c8  bitcoin-0.13.0-x86_64-linux-gnu.tar.gz" > sha256sum.asc
+wget https://github.com/viacoin/viacoin/releases/download/v0.13.3.7/viacoin-0.13.3-x86_64-linux-gnu.tar.gz
+echo "f95915761e0c6803697014a484da9370fbf470972f20144fd37c881207703a61  viacoin-0.13.3-x86_64-linux-gnu.tar.gz" > sha256sum.asc
 sha256sum -c sha256sum.asc
-tar xzvf bitcoin-0.13.0-x86_64-linux-gnu.tar.gz
+tar xzvf viacoin-0.13.3-x86_64-linux-gnu.tar.gz
 popd
 
